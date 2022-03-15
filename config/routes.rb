c179fcc075dd6368e26b9e7318da513a5647064e
anonymous anonymous <mishmosh293@gmail.com>
2022-03-15 01:03:02 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "animal_types#index"
   resources :animals
   resources :animal_types
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
